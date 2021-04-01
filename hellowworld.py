a0518418aadeb9ef1e96e31e1dfc204645258d51
mattB37 <43504472+mattB37@users.noreply.github.com>
2021-04-01 14:42:22 -0500

    Initial Commit

---
+++ hellowworld.py
@@ -0,0 +1,21 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+# print(sys.version)
+# print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://www.berkeley.edu")
+print(r.status_code)
+
+
+# sort imports
+# ctrl + space to autocomplete
