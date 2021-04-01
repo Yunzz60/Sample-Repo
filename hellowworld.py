dec9a2a09c91cbd84066a3c106a6b7189ae77c5c
Matthew Bosch <matthew.bosch37@gmail.com>
2021-04-01 14:52:17 -0500

    Removed some code

--- hellowworld.py
+++ hellowworld.py
@@ -1,23 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-# print(sys.version)
-# print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello {}".format(who_to_greet)
-    return greeting
-
-
-print(greet("Matt"))
 
 r = requests.get("https://www.berkeley.edu")
 print(r.status_code)
-
-
-# sort imports
-# ctrl + space to autocomplete
+print(r.ok)
